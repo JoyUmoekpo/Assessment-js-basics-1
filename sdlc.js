40bff4e1bc188db3a07c8b3b0000c6d7f74ba180
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-11-04 14:32:27 -0500

    Problem 2 Solved

--- sdlc.js
+++ sdlc.js
@@ -17,4 +17,15 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
+
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This is the second step in the software development lifecycle. This step involves analyzing established systems and the functional requirements to ensure the needs of both the customer and end user are met. Possible solutions for these requirements are discussed here.";
+
+const design ="This is the third step in the software development lifecycle. Here developers will create the project's model and infrastructure. This means that developers will determine how the end user will see a given application (front-end) and how the application works behind the scenes (back-end).";
+
+const implementation = "This is the fourth step in the software development lifecycle and where a large portion of the application's coding will take place. Tasks for the project will be divided among the developers to complete different parts of the application.";
+
+const testingAndIntegration = "This is the fifth step in the software development lifecycle. When developers reached this step, they will do a variety of functional and non-functional testing to test the code written in the implementation step. Then, once testing is complete and no bugs are discovered, the application will be deployed for end user use.";
+
+const maintain = "This is the sixth step in the software development lifecycle. During this step, developers will work on various tasks such as working on new features to add to the application, making newer versions of the application as time goes by, and fixing any bugs that were not caught during the fifth step of the process.";
